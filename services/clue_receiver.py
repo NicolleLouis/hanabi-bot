57018622718683f84da4be2c49bdfbe383bc416e
Louis <louisxnicolle@gmail.com>
2024-06-05 20:16:03 +0200

    Start focus implementation

--- services/clue_receiver.py
+++ services/clue_receiver.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from models.game import Game
+    from models.card import Card
 
 
 class ClueReceiver:
@@ -13,7 +14,21 @@ class ClueReceiver:
     def receive_clue(self, data):
         self.save_clue_information(data)
 
-    def save_clue_information(self, data):
+    def touched_cards(self, data) -> List[Card]:
+        cards_touched = []
+        player = self.game.player_finder.get_player(data["player"])
+        for card in player.hand:
+            if card.order in data["list"]:
+                cards_touched.append(card)
+        return cards_touched
+
+    def find_focus(self, data) -> Card:
+        touched_cards = self.touched_cards(data)
+        if len(touched_cards) == 1:
+            return touched_cards[0]
+        raise NotImplementedError("Focus not implemented for multiple cards")
+
+    def save_clue_information(self, data) -> None:
         player = self.game.player_finder.get_player(data["target"])
         is_color_clue = data["clue"]["type"] == 1
         value = data["clue"]["value"]
---
+++ tests/services/test_clue_receiver.py
@@ -0,0 +1,2 @@
+def test_focus():
+    raise NotImplementedError
